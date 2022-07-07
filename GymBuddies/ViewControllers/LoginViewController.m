af6a86686123173c2e06830a9d92b4b39de748fa
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-07-07 14:45:07 -0700

    Log in functionality implemented

--- GymBuddies/ViewControllers/LoginViewController.m
+++ GymBuddies/ViewControllers/LoginViewController.m
@@ -6,9 +6,11 @@
 //
 
 #import "LoginViewController.h"
+#import "Parse/Parse.h"
 
 @interface LoginViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 @end
 
 @implementation LoginViewController
@@ -19,8 +21,64 @@
 }
 
 
+#pragma mark - Button interactions
 
 
+- (IBAction)didTapLogin:(id)sender {
+    if(![self lookForEmptyFields]){
+        NSString *username = self.usernameField.text;
+        NSString *password = self.passwordField.text;
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                [self wrongCredentialsAlert:[error localizedDescription]];
+            } else {
+                
+            }
+        }];
+    }
+    else {
+        [self emptyFieldAlert];
+    }
+}
+
+
+- (IBAction)didTapSignup:(id)sender {
+}
+
+
+
+#pragma mark - Alerts and validations
+
+- (BOOL)lookForEmptyFields{
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    if([username isEqualToString:@""] || [password isEqualToString:@""]){
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+
+-(void)wrongCredentialsAlert:(NSString *)message{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Wrong username or password" message:message preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:okAction];
+
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+-(void)emptyFieldAlert{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Empty field" message:@"There is one or more empty fields" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 /*
 #pragma mark - Navigation
