19ef9b9142e255317a8f7c92cc1ab5ea8228e0c4
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-07-07 15:43:40 -0700

    Register form layout

--- GymBuddies/ViewControllers/RegisterViewController.m
+++ GymBuddies/ViewControllers/RegisterViewController.m
@@ -8,6 +8,10 @@
 #import "RegisterViewController.h"
 
 @interface RegisterViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordField;
 
 @end
 
@@ -15,17 +19,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)didTapSubmit:(id)sender {
 }
-*/
 
 @end
