829a54ab982fca13d631e4d01d5cb82df503f616
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-07-11 15:48:20 -0700

    Basic navigation to the create exercise tab

---
+++ GymBuddies/ViewControllers/CreateExerciseViewController.h
@@ -0,0 +1,16 @@
+//
+//  CreateExerciseViewController.h
+//  GymBuddies
+//
+//  Created by Gael Rodriguez Gomez on 7/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CreateExerciseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GymBuddies/ViewControllers/CreateRoutineViewController.m
@@ -0,0 +1,31 @@
+//
+//  CreateRoutineViewController.m
+//  GymBuddies
+//
+//  Created by Gael Rodriguez Gomez on 7/11/22.
+//
+
+#import "CreateRoutineViewController.h"
+
+@interface CreateRoutineViewController ()
+
+@end
+
+@implementation CreateRoutineViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
